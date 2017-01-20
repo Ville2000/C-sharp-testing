ce84cc1fac295e29ddbd1dd7804e5154b60269a5
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-01-20 19:43:28 +0200

    Add a class Message and a method
    Add a class and a method to the file just to test out a few things.

--- Hello.cs
+++ Hello.cs
@@ -2,6 +2,25 @@ public class HelloWorld
 {
     public static void Main()
     {
+        int value = 10;
+        string name = "Ville";
+        System.Console.WriteLine(name);
         System.Console.WriteLine("Hello, World!");
+        Message msg = new Message();
+        msg.typeHei();
+        System.Console.WriteLine(value);
+        typeWhatYouGet("Moikka!");
+    }
+
+    public static void typeWhatYouGet(string text)
+    {
+        System.Console.WriteLine(text);
+    }
+}
+
+public class Message
+{
+    public void typeHei() {
+        System.Console.WriteLine("Hei!");
     }
 }
